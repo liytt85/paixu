0c832a3659f89b4524207d524e8f492c08155a1a
lytt85 <872149120@qq.com>
2018-04-30 17:54:40 +0800

    tian jia readme

---
+++ test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a;
+	cin >> a;
+	cout << "di yi ci " << endl;
+	cout << "di yi ci " << endl;
+	cout << "di yi ci " << endl;
+	return 0;
+}
